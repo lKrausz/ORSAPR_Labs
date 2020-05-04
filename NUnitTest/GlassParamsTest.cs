fa39fa9ab246a86571409da7dc79bf303eab3697
Vi <krausz.st@gmail.com>
2020-05-04 17:55:02 +0700

    Update GlassParamsTest.cs

--- NUnitTest/GlassParamsTest.cs
+++ NUnitTest/GlassParamsTest.cs
@@ -10,13 +10,6 @@ namespace NUnitTest
         private GlassParams _glass;
         private GlassParams _paperGlass;
 
-        [SetUp]
-        public void Test()
-        {
-            _glass = new GlassParams(15, 20, 45, 3, 3, 3, 3);
-            _paperGlass = new GlassParams(15, 20, 45);
-        }
-
         [Test]
         [TestCase(0.015, 0.02, 0.045, 0.003, 0.003, 0.003, 0.003, TestName = "“ест конструктора дл€ стекл€нного стакана ")]
         public void ConstructorTest(double bottomRadius, double bottomThickness, double height, double topRadius,
